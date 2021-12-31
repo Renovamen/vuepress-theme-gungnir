2d781102f6790644d3f728e3265040085e0032f4
Xiaohan Zou <renovamenzxh@gmail.com>
2021-12-31 20:46:54 +0800

    fix(plugin-giscus): handle ssr

--- packages/plugins/giscus/src/client/clientAppEnhance.ts
+++ packages/plugins/giscus/src/client/clientAppEnhance.ts
@@ -1,6 +1,9 @@
+/* eslint-disable no-undef */
+
 import { defineClientAppEnhance } from "@vuepress/client";
 import Giscus from "./Giscus";
 
 export default defineClientAppEnhance(({ app }) => {
+  if (__VUEPRESS_SSR__) return;
   app.component("Giscus", Giscus);
 });
