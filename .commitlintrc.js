c243f122dbd24af105f8b279a817a4ef0947fff2
zhengqbbb <40693636+Zhengqbbb@users.noreply.github.com>
2022-03-05 13:11:28 +0800

    ci: replace better commitizen adapter (#34)
    * ci: add commitizen adapter: cz-customizable
    
    link #33
    
    * ci: prevent .cz-config.js being formatted
    
    link #33
    
    * ci: use cz-git as commitizen adapter
    
    closed #33
    
    * chore: delete useless config
    
    * ci: no longer depend on the project commitizen
    
    I decided to reduce dependencies `commitizen`
    If user want to use it .Need `npm i -g commitizen`
    
    link #33

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -5,9 +5,13 @@ const plugins = fs
   .readdirSync(path.resolve(__dirname, "packages/plugins"))
   .map((value) => `plugin-${value}`);
 
+/** @type {import('cz-git').UserConfig} */
 module.exports = {
   extends: ["@commitlint/config-conventional"],
   rules: {
     "scope-enum": [2, "always", ["theme", ...plugins]]
+  },
+  prompt: {
+    customScopesAlign: "top"
   }
 };
