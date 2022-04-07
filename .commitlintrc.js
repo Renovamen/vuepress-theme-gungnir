f8f936e4d5ce54fa79e930d54677373f161e19ab
Zhengqbbb <1074059947@qq.com>
2022-04-08 02:41:35 +0800

    chore: update cz-git config and version
    link #51

--- .commitlintrc.js
+++ .commitlintrc.js
@@ -2,15 +2,15 @@ const { execSync } = require("child_process");
 const fs = require("fs");
 const path = require("path");
 
-const plugins = fs
-  .readdirSync(path.resolve(__dirname, "packages/plugins"))
-  .map((value) => `plugin-${value}`);
-
-// branch: feat_36 => #36
+// @example: Branch Name: feature/issue_36 => #36
 const issue = execSync("git rev-parse --abbrev-ref HEAD")
   .toString()
   .trim()
   .split("_")[1];
+
+const plugins = fs
+  .readdirSync(path.resolve(__dirname, "packages/plugins"))
+  .map((value) => `plugin-${value}`);
 
 /** @type {import('cz-git').UserConfig} */
 module.exports = {
@@ -20,6 +20,11 @@ module.exports = {
   },
   prompt: {
     customScopesAlign: "top",
-    defaultIssues: () => (!issue ? "" : `#${issue}`)
+    customIssuePrefixsAlign: !issue ? "top" : "bottom",
+    defaultIssues: !issue ? "" : `#${issue}`,
+    issuePrefixs: [
+      { value: "link", name: "link:     Link to ISSUES" },
+      { value: "closed", name: "closed:   ISSUES has been processed" }
+    ]
   }
 };
