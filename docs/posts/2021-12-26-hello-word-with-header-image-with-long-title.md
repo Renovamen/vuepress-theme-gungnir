d7db28ffaa183193aa245489b79c3a92f2fa9bf8
吴鸿博 <rainbow-w@qq.com>
2022-05-24 22:17:36 +0800

    perf: pager component display logical optimization
    1. according the habit to most people,
       put button "previous" to left and the button "next" to right.
    2. use grid style on Pager component.
    3. add a little animation to these button.
    4. post title inside the button limited to 2 lines.

---
+++ docs/posts/2021-12-26-hello-word-with-header-image-with-long-title.md
@@ -0,0 +1,12 @@
+---
+layout: Post
+title: Hello Word with a long title, it should requires two lines to display.
+subtitle: A Test Post with a Header Image
+author: Renovamen
+date: 2021-12-26
+useHeaderImage: true
+headerImage: /img/in-post/2021-12-25/header.jpg
+headerMask: rgba(40, 57, 101, .4)
+tags:
+  - test
+---
--- packages/theme/src/client/components/Pager.vue
+++ packages/theme/src/client/components/Pager.vue
@@ -1,16 +1,16 @@
 <template>
   <div class="pager">
-    <RouterLink v-if="data.next" class="next" :to="data.next.link">
-      {{ data.next.text }}<br />
-      <span v-if="data.next.title">
-        {{ data.next.title }}
-      </span>
-    </RouterLink>
-
     <RouterLink v-if="data.prev" class="previous" :to="data.prev.link">
       {{ data.prev.text }}<br />
       <span v-if="data.prev.title">
         {{ data.prev.title }}
+      </span>
+    </RouterLink>
+
+    <RouterLink v-if="data.next" class="next" :to="data.next.link">
+      {{ data.next.text }}<br />
+      <span v-if="data.next.title">
+        {{ data.next.title }}
       </span>
     </RouterLink>
   </div>
