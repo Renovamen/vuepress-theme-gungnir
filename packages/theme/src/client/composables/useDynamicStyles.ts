e835aadb97239ab8dcb35a15be0d1a58f26d4c75
rainbowatcher <rainbow-w@qq.com>
2022-07-07 16:58:23 +0800

    fix: scrollbar style in code blocks #77

---
+++ packages/theme/src/client/composables/useDynamicStyles.ts
@@ -0,0 +1,34 @@
+import { onMounted } from "vue";
+
+const codeBlockStyles = `
+pre[class*="language-"] {
+  scrollbar-color: var(--code-scroll-bar-color) rgba(0,0,0,0);
+  scrollbar-width: .5rem;
+}
+
+pre[class*="language-"]::-webkit-scrollbar {
+  height: .5rem;
+}
+
+pre[class*="language-"]::-webkit-scrollbar-thumb {
+  background-color: var(--code-scroll-bar-color);
+  border-radius: 3px;
+}
+
+pre[class*="language-"]::-webkit-scrollbar-thumb:hover {
+  background: var(--c-brand);
+}
+`;
+
+export const setupDynamicStyle = () => {
+  onMounted(() => {
+    if (navigator.userAgent) {
+      const isMobile = navigator.userAgent.includes("Mobi");
+      if (!isMobile) {
+        const style = document.createElement("style");
+        style.textContent = codeBlockStyles;
+        document.head.append(style);
+      }
+    }
+  });
+};
